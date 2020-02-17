c96ac633f98d426002458c284ed9f9ae010f6ff3
Rachel Cherry <bamadesigner@gmail.com>
2020-02-16 21:25:53 -0500

    Adding a space after confirmation messages

--- scripts/bash-functions.sh
+++ scripts/bash-functions.sh
@@ -1,7 +1,7 @@
 # Will print a Y/n confirmation message.
 # Pass the message as the first variable.
 confirm_message() {
-  read -p "$(printf "\n${1} [Y/n]")" yn </dev/tty
+  read -p "$(printf "\n${1} [Y/n] ")" yn </dev/tty
   case $yn in
   [Yy]*) ;;
   [Nn]*) exit ;;
